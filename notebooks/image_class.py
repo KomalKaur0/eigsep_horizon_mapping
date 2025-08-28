e17d327a85069f3a6ea1f01f79d330e8aaddd194
Komal Kaur <komalkaur@berkeley.edu>
2025-08-27 17:10:47 -0700

    valid alt

--- notebooks/image_class.py
+++ notebooks/image_class.py
@@ -1,7 +1,7 @@
-# class to hold image data and metadata
+from eigsep_terrain.marjum_dem import MarjumDEM as DEM
 
 class Image:
-    def __init__(self, pathname, lat=None, lon=None, alt=None, angle_up=None, angle_side=None) -> None:
+    def __init__(self, pathname, label, lat=None, lon=None, alt=None, angle_up=None, angle_side=None) -> None:
         '''
         constructor for image class
 
@@ -9,6 +9,8 @@ class Image:
         ----------
         pathname : str
             path to image
+        label : str
+            name for image
         lat : float, optional
             latitude the photo was taken at
         lon : float, optional
@@ -22,6 +24,7 @@ class Image:
         '''
         
         self.photo = #TODO: read the photo
+        self.label = label
         self.lat = lat
         self.lon = lon
         self.alt = alt
@@ -29,3 +32,47 @@ class Image:
         self.angle_side = angle_side
 
         #TODO: think about storing error bars
+
+    def check_valid_position(self, lat=None, lon=None, alt=None) -> bool:
+        '''
+        checks if a position on the canyon is valid (ie: not falling off a cliff, 
+        entombed in stone, or other such unfortunate places to be taking a photo)
+        via comparison to terrain maps.
+        default paramters are the image's attributes but optional other parameters can be passed in
+
+        parameters
+        ----------
+        lat : float, optional
+            latitude the photo was taken at
+        lon : float, optional
+            longitude the photo was taken at
+        alt : float, optional
+            altitude the photo was taken at
+
+        returns
+        ----------
+        valid : bool
+            True if the position is valid, False if not
+        '''
+
+        #TODO
+        # use the lat and lon to find the correct altitude and see if the given alt is acceptable (+- 5m)
+        if not lat:
+            lat = self.lat
+        if not lon:
+            lon = self.lon
+        if not alt:
+            alt = self.alt
+
+        assert lat is not None
+        assert lon is not None
+        assert alt is not None
+
+        # lla to emu
+        enus = DEM.latlon_to_enu(lat, lon, alt)
+
+        # correct alt
+        interped_alt = DEM.interp_alt(**enus)
+        acceptable_offset = 5 #m
+
+        return interped_alt - acceptable_offset < alt < interped_alt + acceptable_offset
